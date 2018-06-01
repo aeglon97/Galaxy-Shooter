7f0ac359656e998a3e37b6ae103cdc0e91c07fb7
BuildTools <unconfigured@null.spigotmc.org>
2018-06-01 19:42:42 -0400

    PowerUps
    implemented a Triple Shot Power Up and Speed Boost Powerup for the Player

--- Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public bool canTripleShot = false;
+    public bool canSpeedBoost = false;
+
     [SerializeField]
-    private float speed = 5.0f;
+    private float _speed = 6.0f;
+    private float _fireRate = 0.25f;
+    private float _canFire = 0;
+
+    public GameObject laserPrefab;
+    public GameObject tripleShotPrefab;
 
 	// Use this for initialization
 	private void Start ()
@@ -16,6 +25,7 @@ public class Player : MonoBehaviour {
     private void Update()
     {
         Movement();
+        Shoot();
     }
 
     private void Movement()
@@ -24,10 +34,17 @@ public class Player : MonoBehaviour {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-
-        Debug.Log(transform.position);
+        if (canSpeedBoost)
+        {
+            transform.Translate(Vector3.up * _speed * 3.0f * verticalInput * Time.deltaTime);
+            transform.Translate(Vector3.right * _speed * 3.0f * horizontalInput * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
+            transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
+        }
+        
 
         //setting player bounds
         if (transform.position.x < -7.9f)
@@ -48,4 +65,52 @@ public class Player : MonoBehaviour {
             transform.position = new Vector3(transform.position.x, 4, 0);
         }
     }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            if (canTripleShot)
+            {
+                Instantiate(laserPrefab, transform.position + new Vector3(-.7f, 0, 0), Quaternion.identity);
+                Instantiate(laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+                Instantiate(laserPrefab, transform.position + new Vector3(.7f, 0, 0), Quaternion.identity);
+            }
+            //cooldown
+            if (Time.time > _canFire)
+            {
+                _canFire = Time.time + _fireRate;
+                //spawn laser
+                Instantiate(laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    //triple shot logic
+    public void TripleShotOn()
+    {
+        canTripleShot = true;
+        StartCoroutine("TripleShotPowerDownRoutine");
+    }
+
+    public IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        canTripleShot = false;
+    }
+
+    //speed boost logic
+    public void SpeedBoostOn()
+    {
+        canSpeedBoost = true;
+        StartCoroutine("SpeedBoostPowerDownRoutine");
+    }
+
+    public IEnumerator SpeedBoostPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        canSpeedBoost = false;
+    }
 }
+
+
